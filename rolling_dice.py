9f8118d42cd657cfd9897a3d2021a26a437d11d5
Jenny Lee <jennylee.stat@gmail.com>
2017-10-29 22:52:11 -0400

    feat: add sample generator

---
+++ rolling_dice.py
@@ -0,0 +1,42 @@
+from scipy.stats import uniform as unif
+import random
+
+random.seed(42)
+
+two_die = ['Fair', 'Loaded']
+support = [1, 2, 3, 4, 5, 6]
+obsModel = [[1/6, 1/6, 1/6, 1/6, 1/6, 1/6],
+            [1/10, 1/10, 1/10, 1/10, 1/10, 5/10]]
+
+transmat = [[0.95, 0.05],
+            [0.1, 0.90]]
+
+pi = [.5, .5]
+T = 300
+
+def rolling_dice(n=300):
+
+    die = [ ]
+    rolls = [ ]
+
+    # Decide starting dice
+    start_rv = unif.rvs()
+    if start_rv < pi[ 0 ]:
+        dice = 0
+    else:
+        dice = 1
+
+    die.append(two_die[dice])
+
+    for _ in range(n):
+        face = random.choices(support, obsModel[ dice ])[0]
+        rolls.append(face)
+        trans_rv = unif.rvs()
+        if trans_rv < transmat[dice][dice]:
+            dice = dice
+        else:
+            dice ^= 1
+        die.append(two_die[dice])
+
+    return rolls, die
+
